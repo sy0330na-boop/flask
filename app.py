260a1897cd962b0f5790e06ce05ed9329172d400
sy0330na-boop <sy0330na@gmail.com>
2026-05-27 20:55:50 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app=Flask(__name__)
 
 @app.route("/")
-def index():
-    return "Hello Flask"
+def home():
+    print("홈")
+    return render_template("index.html")
+
+@app.route("/aa")
+def aa():
+    print("aa")
+    return"aa"
+
+@app.route("/bb")
+def bb():
+    print("bb")
+    return"bb"
+
+app.run(debug=True)
